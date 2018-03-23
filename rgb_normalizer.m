96c38c60b9fbedb57814d4dec1ff8ae9e5e74307
Gonzalo Solera <gonzalo1796@gmail.com>
2018-03-23 13:35:11 +0100

    Adding rgb light normalizer

---
+++ rgb_normalizer.m
@@ -0,0 +1,17 @@
+function [im_norm] = rgb_normalizer(im)
+    r = im(:, :, 1);
+    g = im(:, :, 2);
+    b = im(:, :, 3);
+    r_norm = double(r) ./ double(r+g+b);
+    r_no_light = r_norm * 255;
+
+    g_norm = double(g) ./ double(r+g+b);
+    g_no_light = g_norm * 255;
+    
+    b_norm = double(b) ./ double(r+g+b);
+    b_no_light = b_norm * 255;
+
+    im_norm = cat(3, r_no_light, g_no_light, b_no_light);
+    
+end
+
