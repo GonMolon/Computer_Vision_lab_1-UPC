3704627e8b3f5d6498cfe8d3348775ef0b862b73
Gonzalo Solera <gonzalo1796@gmail.com>
2018-03-25 21:26:17 +0200

    Starting main implementation

--- main.m
+++ main.m
@@ -1,2 +1,22 @@
-model = create_model(@rgb_normalizer);
+clear all
+close all
 
+normalizer = @rgb_normalizer;
+
+model_images = ["05.jpg"];
+
+model = create_model(normalizer);
+
+images = dir('data/*/*.jpg');
+correct_predictions = 0;
+for image = images'
+    
+    aux = regexp(image.folder,'/','split');
+    aux = aux(end);
+    team = aux{1};
+    
+    if ~ismember(image.name, model_images) || ~strcmp(team, 'barcelona')
+        im = imread(strcat(image.folder, '/', image.name));
+    end      
+    
+end
