e62fcb5eedaa6f95e0023f19d7dfb8bdb9daa121
Gonzalo Solera <gonzalo1796@gmail.com>
2018-03-27 13:59:43 +0200

    Euclidean distance histogram comparison

--- get_histograms.m
+++ get_histograms.m
@@ -1,6 +1,5 @@
 function [red_hist, blue_hist] = get_histograms(im)
     global BIN_SIZE
-    BIN_SIZE
     red_hist = histcounts(im(:, :, 1), 255/BIN_SIZE, 'Normalization', 'probability');
     blue_hist = histcounts(im(:, :, 2), 255/BIN_SIZE, 'Normalization', 'probability');
 end
