d5afaf4e56097646041300025a35b55cbc75e5df
Gonzalo Solera <gonzalo1796@gmail.com>
2018-03-29 12:28:04 +0200

    Changing to vectorized implementation

--- comp_hist_euclidean.m
+++ comp_hist_euclidean.m
@@ -1,7 +1,4 @@
 function [diff] = comp_hist_euclidean(hist1, hist2)
-    diff = 0;
-    for i = [hist1; hist2]
-        diff = diff + (i(1) - i(2))^2;
-    end
+    diff = sum((hist1 - hist2).^2);
 end
 
