fc7e4d9654b8b448aeff1d7fa1be82c29e49af4c
Gonzalo Solera <gonzalo1796@gmail.com>
2018-04-24 16:27:42 +0200

    Improving black and white projection in histogram

--- get_hsv_histogram.m
+++ get_hsv_histogram.m
@@ -4,14 +4,14 @@ function [h] = get_hsv_histogram(im)
     im_hsl(:, :, 1) = im_hsl(:, :, 1)/360;
     for i = 1 : size(im_hsl, 1)
         for j = 1 : size(im_hsl, 2)
-            if im_hsl(i, j, 3) < 0.1
-                im_hsl(i, j, 1) = 0.75;
-                im_hsl(i, j, 2) = -im_hsl(i, j, 2);
-            elseif im_hsl(i, j, 3) > 0.95
-                im_hsl(i, j, 1) = 0.25;
-                im_hsl(i, j, 2) = -im_hsl(i, j, 2);
+            if im_hsl(i, j, 3) < 0.2
+                im_hsl(i, j, 1) = 0.7 + im_hsl(i, j, 1)*0.1;
+                im_hsl(i, j, 2) = -0.5;
+            elseif im_hsl(i, j, 3) > 0.85
+                im_hsl(i, j, 1) = 0.2 + im_hsl(i, j, 1)*0.1;
+                im_hsl(i, j, 2) = -0.5;
             end
         end
     end
-    h = histcounts2(im_hsl(:, :, 1), im_hsl(:, :, 2), N_BINS, 'Normalization', 'probability', 'XBinLimits', [0, 1], 'YBinLimits', [-1, 1]);
+    h = histcounts2(im_hsl(:, :, 1), im_hsl(:, :, 2), N_BINS, 'Normalization', 'probability', 'XBinLimits', [0, 1], 'YBinLimits', [-0.5, 1]);
 end
